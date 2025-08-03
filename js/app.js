0a0cd72311e52f543aefc1f9beb2fc7b2e51d689
Tali <talicabana@gmail.com>
2025-08-03 11:18:01 -0300

    Podemos abrir la ventana modal desde javascript

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // elementos del DOM
 const btnAgregarContacto = document.getElementById("btnAgregarContacto");
-
+const modalFormularioContacto = new bootstrap.Modal (document.getElementById("contactoModal"))
 
 // manejadores de eventos
 btnAgregarContacto.addEventListener("click",()=>{
-    
+    modalFormularioContacto.show()
 })
