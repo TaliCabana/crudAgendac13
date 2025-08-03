1c9e54ac620f4d3850e90efa7dc297a9b0430264
Tali <talicabana@gmail.com>
2025-08-03 11:34:39 -0300

    Agregamos el evento submit al formulario de contactos

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 // elementos del DOM
 const btnAgregarContacto = document.getElementById("btnAgregarContacto");
 const modalFormularioContacto = new bootstrap.Modal (document.getElementById("contactoModal"))
+// "new" crea un objeto de la clase, cuál clase? la clase creada por bootstrap (bootstrap.Modal), y de esta manera podemos usar los métodos y las propiedades que figuran en la tablita del final del modal. Yo lo que necesito es usar el "show"
+// (document.getElementById("contactoModal") esto es el constructor, yo le estoy diciendo: construime el modal que he dejado maquetado
+const formularioContacto = document.getElementById("formContacto")
+
+// Funnciones
+const crearContacto = ()=>{
+    console.log("Aquí tengo que crear el contacto")
+}
 
 // manejadores de eventos
 btnAgregarContacto.addEventListener("click",()=>{
     modalFormularioContacto.show()
+})
+
+formularioContacto.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    // Aquí tengo que crear / editar un contacto
+    crearContacto()
 })
