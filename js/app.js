bfc026d5629380c35af1f5c4308245c647be21d4
Tali <talicabana@gmail.com>
2025-08-03 22:46:50 -0300

    Agregamos sweet alert cuando el contacto fue creado

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ import Contacto from "./contacto.js";
 
 // Todas las variables que son elementos del DOM
 const btnAgregarContacto = document.getElementById("btnAgregarContacto");
-const modalFormularioContacto = new bootstrap.Modal (document.getElementById("contactoModal"))
+const modalFormularioContacto = new bootstrap.Modal(
+  document.getElementById("contactoModal")
+);
 // "new" crea un objeto de la clase, cuál clase? la clase creada por bootstrap (bootstrap.Modal), y de esta manera podemos usar los métodos y las propiedades que figuran en la tablita del final del modal. Yo lo que necesito es usar el "show"
 // (document.getElementById("contactoModal") esto es el constructor, yo le estoy diciendo: construime el modal que he dejado maquetado
-const formularioContacto = document.getElementById("formContacto")
+const formularioContacto = document.getElementById("formContacto");
 const inputNombre = document.getElementById("nombre");
 const inputApellido = document.getElementById("apellido");
 const inputEmail = document.getElementById("email");
@@ -17,38 +19,53 @@ const inputPuestoTrabajo = document.getElementById("puestoTrabajo");
 const inputEmpresa = document.getElementById("empresa");
 const agenda = JSON.parse(localStorage.getItem("agendaKey")) || [];
 
-
 // Funnciones
-const guardarLocalstorage = () =>{
-    localStorage.setItem("agendaKey", JSON.stringify(agenda))
-}
+const guardarLocalstorage = () => {
+  localStorage.setItem("agendaKey", JSON.stringify(agenda));
+};
 
-const crearContacto = ()=>{
-    console.log("Aquí tengo que crear el contacto")
-    // todo Agregar validaciones 
-    // buscar los datos del formulario y crear un objeto contacto
-    const contactoNuevo = new Contacto(inputNombre.value, inputApellido.value, inputTelefono.value, inputEmail.value, inputImagen.value, inputEmpresa.value, inputPuestoTrabajo.value, inputDireccion.value, inputNotas.value)
-    // guardar el contacto en la agenda de contactos
-    agenda.push(contactoNuevo)
-    console.log(contactoNuevo)
-    // guardar la agenda en el localstorage   
-    guardarLocalstorage();
-    // mostrar un mensaje al usuario final
-    // limpiar el formulario
-    limpiarFormulario()
-}
+const crearContacto = () => {
+  console.log("Aquí tengo que crear el contacto");
+  // todo Agregar validaciones
+  // buscar los datos del formulario y crear un objeto contacto
+  const contactoNuevo = new Contacto(
+    inputNombre.value,
+    inputApellido.value,
+    inputTelefono.value,
+    inputEmail.value,
+    inputImagen.value,
+    inputEmpresa.value,
+    inputPuestoTrabajo.value,
+    inputDireccion.value,
+    inputNotas.value
+  );
+  // guardar el contacto en la agenda de contactos
+  agenda.push(contactoNuevo);
+  console.log(contactoNuevo);
+  // guardar la agenda en el localstorage
+  guardarLocalstorage();
+  // mostrar un mensaje al usuario final
+  Swal.fire({
+    title: "Contacto creado",
+    text: `El contacto ${inputNombre.value} fue creado correctamente`,
+    icon: "success",
+    confirmButtonText: "Ok",
+  });
+  // limpiar el formulario
+  limpiarFormulario();
+};
 
-function limpiarFormulario(){
-    formularioContacto.reset()
+function limpiarFormulario() {
+  formularioContacto.reset();
 }
 
 // manejadores de eventos
-btnAgregarContacto.addEventListener("click",()=>{
-    modalFormularioContacto.show()
-})
+btnAgregarContacto.addEventListener("click", () => {
+  modalFormularioContacto.show();
+});
 
-formularioContacto.addEventListener("submit", (e)=>{
-    e.preventDefault()
-    // Aquí tengo que crear / editar un contacto
-    crearContacto()
-})
+formularioContacto.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Aquí tengo que crear / editar un contacto
+  crearContacto();
+});
